ab7168ae321631fe6417ab24937398671e2b12e5
Jatin Wadhwani <jatinwadhwaniofficial1@gmail.com>
2025-05-28 01:45:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
 
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAEj68tWEvloU5DWtWVyeyt8IDR60XOLR4",
+    authDomain: "ndhwp-portal.firebaseapp.com",
+    projectId: "ndhwp-portal",
+    storageBucket: "ndhwp-portal.firebasestorage.app",
+    messagingSenderId: "28034201169",
+    appId: "1:28034201169:web:586843bf9f145a02152a70"
+};
